da36f8c1dc68c8925206e0ff451850f282461150
Logan <logancoghill18@gmail.com>
2021-01-21 14:11:55 +0200

    updated notes(not finished)

--- 04-Reat.jsx
+++ 04-Reat.jsx
@@ -6,28 +6,32 @@
 
 {/*JSX is similar to HTML to a certain degree, there are key differences that will be covered. for instance, because JSX is a syntactic extention of JS, this means that you can add JS directly with JSX by putting the code in {}. Rememeber this for later.*/}
 
+{/*how to create comments in JSX*/}
+
 {/*here is an example of making html elements with js*/}
 const HelloJSX = <h1>Hello JSX!</h1>;
 {/*the above is know as a simple JSX element*/}
 
 {/*more complex elements can be created, but you must note that you must return one element*/}
-const DIV =(
-    <div>
-        <h2>I'm a Sub-heading :|</h2>
-        <p>text is what I am...</p>
-    </div>
-);
+  const DIV = () => {
+    return (
+        <div>
+            <h2>I'm a Sub-heading :|</h2>
+            <p>text is what I am...</p>
+        </div>
+    );
+};
 
 {/*as you can see we have multiple elements, but we are only returning the Div that holds those elements*/}
 
-{/*is used to create comments in JSX,:{/**/}
-const JSX = (
-  <div>
-    <h2>There is more!</h2>
-    <p>Words go here...</p>
-    {/*this is a comment*/}
-  </div>
-);
+const JSX = () => {
+  return (
+     <div>
+        <h2>There is more!</h2>
+        <p>Words go here...</p>
+     </div>
+  );
+};
 
 {/*So, JSX is a conveinient tool to write HTML within JavaScript. Using React we can render this JSX directly to the HTML DOM using React's rendering API known as ReactDOM*/}
 {/*ReactDOM offers a simmple method to render React elements to the DOM*/}
@@ -44,24 +48,30 @@ ReactDOM.render(HelloJSX,document.getElementById("main-heading"));
 {/*onClick instead of onclick*/}
 {/*onChange instead of onchange*/}
 {/*the difference might be small but is something to keep in mind*/}
-const aClassDiv = (
-  <div className ="myDiv">
+const MoreHere = () => {
+ return (
+    <div className ="myDiv">
     <h1>Im in a class!</h1>
     <p>you dont know my class...</p>
   </div>
-);
+ );
+};
 
 {/*another difference is that HTML has Self-closing tags, such as <br> and <hr>. in JSX this would cause an error, all tags must be closed*/}
 {/*for HTML self-closing tags, in JSX you can write the with the following syntax*/}
-const CloseME = (
-  <div>
-    <h2>Welcome to React!</h2>
-    <br />
-    {/*Notice the space before the "/" */}
-    <p>Be sure to close all tags!</p>
-    <hr />
-  </div>
-);
+const CloseMe = ()=>{
+ return(
+      <div>
+        <h2>Welcome to React!</h2>
+        <br />
+        {/*Notice the space before the "/" */}
+        <p>Be sure to close all tags!</p>
+        <hr />
+      </div>
+ );
+ 
+  
+};
 
 {/*Extra Info: the div tag is not self-closing but can also be written as <div />, all this will do is make sure that you cannot include anything within that div. later you will see why this is useful for rendering react components.*/}
 
@@ -105,15 +115,73 @@ class Puppies extends React.Component {
 
   render() {
     return (
-    <App>
+    <div>
     {/*When react encounters a custom tag that references another component, it renders that component in the location of the tag*/}
         <DIV />
         <JSX />
-        <aClassDiv />
-    </App>
+        <MoreHere />
+    </div>
     );
   };
 
 };
 
 {/*Component composition is very powerful, it is important to think of your UI in terms of components. you break down your UI into its basic building blocks(they become the components). this greatly simplifies the development and maintenance of complex projects */}
+
+{/*to render the ES6 class you can use the same syntax as before. the only difference is that when you call a ES6 class you must wrap its name in a tag(it becomes a custom tag) */}
+ReactDOM.render(<Puppies />,document.getElementById("body-text"));
+{/*See it work on codepen: https://codepen.io/Logan_code/pen/BaLMwdX */}
+
+{/* just a recap, simple components are rendered with thier variable name such as the HelloJSX at the top of the document. for ES6 class components and functional components we use the custom tag, as show with the Puppies class*/}
+
+{/*React components are (most of the time) written in ES6 class syntax, the more you practise them the more complex you can make them */}
+class MyComponent extends React.Component{
+    constructor(props){
+        super(props);
+    };
+    
+    render() {
+        return(
+            <div>
+                <CloseMe />
+                <Kitten />
+            </div>
+        );
+    };
+};
+ReactDOM.render(<MyComponent />,document.getElementById("more-text"));
+{/*See it work on codepen: https://codepen.io/Logan_code/pen/BaLMwdX */}
+
+{/*So we have learned about the different types of components (simple, functional and ES6 class) */}
+{/*Props or properties can be used as a parameter for functional components*/}
+const Welcome = (props) =>{
+    return (
+    <p>hello {props.user}</p>
+    );
+};
+{/*the above means, when the custom tag is rendered, whatever you set the "user" property to will be used as the value*/}
+const element = <Welcome user="logan"/>;
+ReactDOM.render(element,document.getElementById("myName"));
+{/*See it work on codepen: https://codepen.io/Logan_code/pen/BaLMwdX */}
+
+{/*you can also pass an array as props/properties*/}
+const MyArr = (props)=>{
+    return (
+        <p>{props.color.join(", ")}</p>
+    );
+};
+{/*the above takes the array given to color, joins it into a string separated by a "," */}
+
+const RGB = <MyArr color={["red","green","blue"]} />;
+ReactDOM.render(RGB,document.getElementById("RGB"));
+{/*See it work on codepen: https://codepen.io/Logan_code/pen/BaLMwdX */}
+
+{/*You can also set default props. theses values are used when you dont specify*/}
+Welcome.defaultProps(user:'No_User');
+{/*so if you render the welcome component but dont give it a user value, it will use the above*/}
+{/*to override the default, just give "user" a value when calling the component*/}
+ReactDOM.render(Welcome,document.getElementById("not_my_name"));
+{/*See it work on codepen: https://codepen.io/Logan_code/pen/BaLMwdX */}
+
+{/*if you want the value of props to be integer instead of string, wrap the value in {} */}
+ReactDOM.render(<Welcome user={100}/>,document.getElementById("not_a_name"));
